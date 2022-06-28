7e3c3599706a17a768f18146668e9c706e0d03e2
wei_ming_kng <weiming.kng@gmail.com>
2022-06-28 21:33:25 +0800

    include function for constituency extraction

---
+++ extract_constituents.py
@@ -0,0 +1,21 @@
+def get_constituents(sentence: str, brackets: str = "()") -> list:
+    """
+    Uses a sliding window to extract the constituents of a sentence in treebank format.
+    """
+    constituency = list()
+    sentence_len = len(sentence)
+
+    for i in range(sentence_len):
+        # If match the first bracket, move onto the next char
+        if sentence[i] == brackets[0]:
+
+            for j in range(i + 1, sentence_len):
+                # Check for start of the string if it matches another open bracket
+                if sentence[j] == brackets[0]:
+                    # Append the constituent to the list
+                    constituency.append(sentence[i + 1 : j - 1])  # Remove extra space
+                    break
+
+                elif sentence[j] == brackets[1]:
+                    break
+    return constituency
