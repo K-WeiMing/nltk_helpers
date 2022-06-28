fb858bb423851bc9d3aaaa8c865affdaa7976599
wei_ming_kng <weiming.kng@gmail.com>
2022-06-28 21:39:50 +0800

    function to count pos tags

---
+++ count_pos_tags.py
@@ -0,0 +1,28 @@
+from nltk.corpus.reader import BracketParseCorpusReader
+import pandas as pd
+
+
+def count_pos_tags(reader: BracketParseCorpusReader) -> pd.DataFrame:
+    """
+    Generate DataFrame of Parts-of-Speech tags in a corpus
+
+    Args:
+        reader (BracketParseCorpusReader): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    pos_counter = dict()
+
+    for word, tag in reader.tagged_words():
+        if tag in pos_counter:
+            pos_counter[tag] += 1
+        else:
+            pos_counter[tag] = 1
+    df = pd.DataFrame(pos_counter, index=["counts"])
+    df = (
+        df.T.sort_values(by="counts", ascending=False)
+        .reset_index()
+        .rename(columns={"index": "pos_tag"})
+    )
+    return df
