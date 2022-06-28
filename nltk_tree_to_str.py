2b17f392bee3ef8989500d2eddfeea69954a7939
wei_ming_kng <weiming.kng@gmail.com>
2022-06-28 21:19:57 +0800

    update gitignore and nltk function description

--- nltk_tree_to_str.py
+++ nltk_tree_to_str.py
@@ -2,17 +2,21 @@ import nltk
 
 Treebank: nltk.Tree
 
-def convert_tree_to_str(tb: Treebank):
-    """Converts Treebank object to string representation
+
+def convert_tree_to_str(tb: Treebank) -> str:
+    """
+    Converts Treebank object to string representation
 
     Args:
-        tb (Treebank): _description_
+        tb (Treebank): nltk.Tree object
+
+    Returns:
+        str: String representation (Treebank Format) from nltk.Tree object
     """
-    
     treebank_list = []
-    for t in tb.pformat().split('\n'):
-        treebank_list.append(' ' + t.lstrip())
-        
-    treebank_str = ''.join(treebank_list)
+    for t in tb.pformat().split("\n"):
+        treebank_list.append(" " + t.lstrip())
+
+    treebank_str = "".join(treebank_list)
 
     return treebank_str.strip()
