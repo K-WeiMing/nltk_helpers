0cac1d0f8151fd8d810dfffc16c614ebbe7b6153
wei_ming_kng <weiming.kng@gmail.com>
2022-12-13 01:12:42 +0800

    update variable name

--- src/nltk_tree_to_str.py
+++ src/nltk_tree_to_str.py
@@ -3,7 +3,7 @@ import nltk
 Treebank: nltk.Tree
 
 
-def convert_tree_to_str(tb: Treebank) -> str:
+def convert_tree_to_str(tree_bank: Treebank) -> str:
     """
     Converts Treebank object to string representation
 
@@ -14,8 +14,8 @@ def convert_tree_to_str(tb: Treebank) -> str:
         str: String representation (Treebank Format) from nltk.Tree object
     """
     treebank_list = []
-    for t in tb.pformat().split("\n"):
-        treebank_list.append(" " + t.lstrip())
+    for tree in tree_bank.pformat().split("\n"):
+        treebank_list.append(" " + tree.lstrip())
 
     treebank_str = "".join(treebank_list)
 
