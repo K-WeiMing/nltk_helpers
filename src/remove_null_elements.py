6d128f786880509d0a3a77132990b137fd75b75c
wei_ming_kng <weiming.kng@gmail.com>
2022-12-13 01:43:37 +0800

    update docstrings

--- src/remove_null_elements.py
+++ src/remove_null_elements.py
@@ -5,12 +5,25 @@ from nltk.tree import collapse_unary
 """
 Example input from Kethu dataset
 
-(S (NP-SBJ (NP (NN Jenis) (NN monyet)) (SBAR (IN yang) (S (NP-SBJ-1 (-NONE- *)) (VP (VB dikerahkan) (NP (-NONE- *-1)) (NP-LGS (NN pemkot)))))) (VP (VP (VB berbadan) (ADJP (JJ besar))) (, ,) (VP (VB berekor) (ADJP (JJ panjang))) (, ,) (CC dan) (VP (VB memiliki) (NP (NN wajah) (SBAR (-NONE- 0) (S (NP-SBJ (-NONE- *)) (VP (VB berwarna) (ADJP (JJ hitam)))))))) (. .))
+(S (NP-SBJ (NP (NN Jenis) (NN monyet)) (SBAR (IN yang) 
+(S (NP-SBJ-1 (-NONE- *)) (VP (VB dikerahkan) (NP (-NONE- *-1)) 
+(NP-LGS (NN pemkot)))))) (VP (VP (VB berbadan) (ADJP (JJ besar))) 
+(, ,) (VP (VB berekor) (ADJP (JJ panjang))) (, ,) 
+(CC dan) (VP (VB memiliki) (NP (NN wajah) (SBAR (-NONE- 0) 
+(S (NP-SBJ (-NONE- *)) (VP (VB berwarna) (ADJP (JJ hitam)))))))) (. .))
 
 """
 
 
-def remove_none(input_str):
+def remove_none(input_str: str) -> str:
+    """Remove null elements from the input string
+
+    Args:
+        input_str (str): Input constituency treebank in string format
+
+    Returns:
+        _type_: Output constituency treebank without null elements
+    """
 
     none_count = input_str.count("(-NONE- ")
 
@@ -36,13 +49,28 @@ def remove_none(input_str):
     return res
 
 
-def remove_no_leaves(input_str):
+def remove_no_leaves(input_str: str) -> str:
+    """Replace the treebank structure using regex substitution
+
+    Args:
+        input_str (str): Input constituency treebank with incorrect format (e.g. "(NP-SBJ )")
+
+    Returns:
+        str: Output constituency treebank with corrected format
+    """
     res = re.sub(r"\([a-zA-Z\-1-9\*]*\s\)", "", input_str)
     return res
 
 
-def remove_null_elements(input_string):
+def remove_null_elements(input_string: str) -> str:
+    """Remove all null elements from the treebank
 
+    Args:
+        input_string (str): Input constituency treebank in string format
+
+    Returns:
+        str: Output final constituency treebank with null elements removed
+    """
     # Step 1: Remove -NONE- pos tags
     remove_none_tags = remove_none(input_string)
 
