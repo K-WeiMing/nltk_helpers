6d6d56876cc0b2a7851e75b59bba03b894951533
wei_ming_kng <weiming.kng@gmail.com>
2022-12-13 01:13:24 +0800

    update list declaration

--- src/extract_constituents.py
+++ src/extract_constituents.py
@@ -2,7 +2,7 @@ def get_constituents(sentence: str, brackets: str = "()") -> list:
     """
     Uses a sliding window to extract the constituents of a sentence in treebank format.
     """
-    constituency = list()
+    constituency = []
     sentence_len = len(sentence)
 
     for i in range(sentence_len):
